bee4d586912a4cbc27e46bfe2060a3df87d2dc51
LiJohnson <598420668@163.com>
2020-05-20 21:18:46 +0800

    feat: 调整消息发送格式

--- src/main/java/com/ztbsuper/wechatwork/JobListener.java
+++ src/main/java/com/ztbsuper/wechatwork/JobListener.java
@@ -33,14 +33,15 @@ public class JobListener extends RunListener<AbstractBuild> {
 		Result result = build.getResult();
 		if (!checkSendMessage(result, wechatWorkNotifier)) return;
 
-		String status = Result.SUCCESS.equals(result) ? "✅" : Result.FAILURE.equals(result) ? "‼️" : "⚠️";
+		String status = Result.SUCCESS.equals(result) ? "✅" : Result.FAILURE.equals(result) ? "🚫️" : "⚠️";
 		String node = build.getBuiltOn().getNodeName();
 		String desc = String.format(
-				"### desc \n\n- node : %s\n- summary : %s\n- duration : %s", StringUtils.isBlank(node) ? "master" : node,
+				"> **desc** `%s` `%s` `%s`",
+				StringUtils.isBlank(node) ? "master" : node,
 				build.getBuildStatusSummary().message,
 				build.getDurationString()
 		);
-		String message =  String.format("# %s【%s】build %s\n\n%s\n\n%s", status, build.getProject().getDisplayName(), result == null ? "UNKNOWN" : result, desc, getBuildInfo(build));
+		String message =  String.format("## %s【%s】build %s\n%s\n%s", status, build.getProject().getDisplayName(), result == null ? "UNKNOWN" : result, desc, getBuildInfo(build));
 
 		wechatWorkNotifier.sendMessage(message);
 	}
@@ -60,7 +61,7 @@ public class JobListener extends RunListener<AbstractBuild> {
 		if(isDefaultMatrixProjectName(build)) return;
 		WechatWorkNotifier wechatWorkNotifier = getService(build);
 		if (wechatWorkNotifier.getOnStart()) {
-			String content = String.format("# 🙏🏻【%s】 build started\n\n%s",
+			String content = String.format("## 🙏🏻【%s】 build started\n\n%s",
 					build.getProject().getDisplayName(),
 					getBuildInfo(build));
 			wechatWorkNotifier.sendMessage(content);
@@ -69,22 +70,28 @@ public class JobListener extends RunListener<AbstractBuild> {
 
 	private static String getBuildInfo(AbstractBuild build){
 		String params = build.getActions(ParametersAction.class).stream()
-				.map(parameterValues -> parameterValues.getAllParameters().stream().map(parameterValue -> String.format("- %s : %s", parameterValue.getName(), parameterValue.getValue())).collect(Collectors.joining("\n")))
+				.map(parameterValues -> parameterValues.getAllParameters()
+						.stream()
+						.filter(parameterValue -> !"build_node".equals(parameterValue.getName()))
+						.map(parameterValue -> String.format("> %s : `%s`", parameterValue.getName(), parameterValue.getValue()))
+						.collect(Collectors.joining("\n")))
 				.collect(Collectors.joining("\n"));
-
 		String url = String.format("%sjob/%s/%s/console", JenkinsLocationConfiguration.get().getUrl(), build.getProject().getDisplayName(), build.getNumber());
-		return String.format("### params \n%s \n\n### [build(%s)](%s)\n\n %s", params, build.getDisplayName(), url, url);
+		if(StringUtils.isBlank(params)){
+			params = "> 🈚️";
+		}
+		return String.format("> **params** \n%s \n> **[build(%s)](%s)**\n>%s", params, build.getDisplayName(), url, url);
 	}
 
 	private WechatWorkNotifier getService(AbstractBuild build) {
-		Optional a =build.getProject()
+		Optional optional = build.getProject()
 				.getPublishersList()
 				.toMap()
 				.values()
 				.stream()
 				.filter(publisher -> publisher instanceof WechatWorkNotifier)
 				.findFirst();
-		Assert.isTrue(a.isPresent(), "没有找到 WechatWorkNotifier");
-		return (WechatWorkNotifier) a.get();
+		Assert.isTrue(optional.isPresent(), "没有找到 WechatWorkNotifier");
+		return (WechatWorkNotifier) optional.get();
 	}
 }
